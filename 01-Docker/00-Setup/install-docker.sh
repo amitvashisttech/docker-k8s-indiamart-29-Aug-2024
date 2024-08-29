875ae78c95cf33708979ee131869ea4e579bb42e
root <root@worker1.example.com>
2024-08-29 06:50:03 +0000

    Docker - Setup

---
+++ 01-Docker/00-Setup/install-docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "********************  Installing Docker ***********************"
+sleep 3
+apt-get update
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+apt-get update && apt-get install -y docker-ce
+
